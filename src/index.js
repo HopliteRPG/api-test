80c4f9152b66a78e02bc0f31dc0910344ecd7502
Bill <146594930+HopliteRPG@users.noreply.github.com>
2025-09-28 20:30:49 -0400

    moved search gif to the form folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 import { randomGifFunc } from "./randomBtn";
 import { catOnStartUp } from "./randomCat";
-import { searchForGif } from "./searchGif";
+import { searchForGif } from "./formCode/searchGif";
 import { createSearchForm } from "./formCode/formHtml";
 catOnStartUp();
 randomGifFunc();
