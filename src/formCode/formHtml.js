246ee25adfcc22beb68ec3e2a50604a615147955
Bill <146594930+HopliteRPG@users.noreply.github.com>
2025-09-28 20:10:46 -0400

    added dialog create function

---
+++ src/formCode/formHtml.js
@@ -0,0 +1,84 @@
+const body = document.querySelector("body");
+const dialogCreate = document.createElement("dialog");
+const formCreate = document.createElement("form");
+const fieldSetCreate = document.createElement("fieldset");
+const legendCreate = document.createElement("legend");
+const labelCreate = document.createElement("label");
+const inputCreate = document.createElement("input");
+const divCreate = document.createElement("div");
+const buttonCreate = document.createElement("button");
+const pCreate = document.createElement("p");
+
+function createDialog(parentDiv) {
+  let cloneDialogCreate = dialogCreate.cloneNode(true);
+  parentDiv.appendChild(cloneDialogCreate);
+  return { cloneDialogCreate };
+}
+
+function createForm(parentDiv) {
+  let cloneFormCreate = formCreate.cloneNode(true);
+  parentDiv.appendChild(cloneFormCreate);
+  return { cloneFormCreate };
+}
+
+function createFieldSet(form) {
+  let cloneFieldSetCreate = fieldSetCreate.cloneNode(true);
+
+  form.appendChild(cloneFieldSetCreate);
+  return { cloneFieldSetCreate };
+}
+
+function createLegend(fieldset, text) {
+  let cloneLegendCreate = legendCreate.cloneNode(true);
+  cloneLegendCreate.textContent = text;
+  console.log(cloneLegendCreate);
+  fieldset.appendChild(cloneLegendCreate);
+  return { cloneLegendCreate };
+}
+
+function createLabel(div, className, text) {
+  let cloneLabelCreate = labelCreate.cloneNode(true);
+  cloneLabelCreate.classList.add(className);
+  cloneLabelCreate.textContent = text;
+  div.appendChild(cloneLabelCreate);
+  return { cloneLabelCreate };
+}
+
+function createInput(div, type, name, requiredOrNot) {
+  let cloneInputCreate = inputCreate.cloneNode(true);
+  cloneInputCreate.classList.add(name);
+  div.appendChild(cloneInputCreate);
+  cloneInputCreate.type = type;
+  cloneInputCreate.setAttribute("name", name);
+
+  if (requiredOrNot === true) {
+    cloneInputCreate.required = true;
+  } else if (requiredOrNot === false) {
+    cloneInputCreate.required = false;
+  }
+  return { cloneInputCreate };
+}
+
+function createDivSection(parentDiv, className) {
+  let cloneDivCreate = divCreate.cloneNode(true);
+  cloneDivCreate.classList.add(className);
+  parentDiv.appendChild(cloneDivCreate);
+  return { cloneDivCreate };
+}
+
+function createButton(div, buttonText, buttonClass) {
+  let cloneButtonCreate = buttonCreate.cloneNode(true);
+  cloneButtonCreate.innerText = buttonText;
+  cloneButtonCreate.classList.add(buttonClass);
+  div.appendChild(cloneButtonCreate);
+  return { cloneButtonCreate };
+}
+
+function createP(div, pText, pClass) {
+  let clonePCreate = pCreate.cloneNode(true);
+  clonePCreate.innerText = pText;
+  clonePCreate.classList.add(pClass);
+  div.appendChild(clonePCreate);
+}
+
+function createSearchForm() {}
