2756aaa642cbc2bc83b92abae3f55c7c8f119228
Bill <146594930+HopliteRPG@users.noreply.github.com>
2025-10-06 21:30:47 -0400

    random button working

--- src/randomBtn.js
+++ src/randomBtn.js
@@ -1,18 +1,18 @@
-// export { randomGifFunc };
-// const img = document.querySelector("img");
-// const randomGifBtn = document.querySelector(".random-gif-btn");
+export { randomGifFunc };
 
-// function randomGifFunc() {
-//   randomGifBtn.addEventListener("click", () => {
-//     fetch(
-//       "https://api.giphy.com/v1/gifs/random?api_key=iynhuQUGqRgxLYzfuv3bOxaTvkLzKHeY",
-//     )
-//       .then(function (response) {
-//         console.log(response.clone().json());
-//         return response.clone().json();
-//       })
-//       .then(function (response) {
-//         img.src = response.data.images.original.url;
-//       });
-//   });
-// }
+function randomGifFunc() {
+  const displayImg = document.querySelector(".displayImg");
+  const randomGifBtn = document.querySelector(".random-gif-btn");
+  randomGifBtn.addEventListener("click", () => {
+    fetch(
+      "https://api.giphy.com/v1/gifs/random?api_key=iynhuQUGqRgxLYzfuv3bOxaTvkLzKHeY",
+    )
+      .then(function (response) {
+        console.log(response.clone().json());
+        return response.clone().json();
+      })
+      .then(function (response) {
+        displayImg.src = response.data.images.original.url;
+      });
+  });
+}
